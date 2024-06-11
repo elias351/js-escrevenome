402c1fe15dae0ca2e430c1d8099dcbe0409df363
elias351 <00001071087496sp@al.educacao.sp.gov.br>
2024-06-11 15:39:31 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let cor;
+
+function setup() {
+
+    createCanvas(600, 600);
+    background("black");
+    }
+    
+function draw() {
+  stroke("blue");
+    fill("red");
+  
+  if(mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
+
+
